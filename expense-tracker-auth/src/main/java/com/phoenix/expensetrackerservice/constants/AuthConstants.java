0b20319e85597f57aadefa09fb64fe545fcb4f22
apassan23 <abhishekpassan99@gmail.com>
2023-10-02 22:23:49 +0530

    added actuator endpoints

--- expense-tracker-auth/src/main/java/com/phoenix/expensetrackerservice/constants/AuthConstants.java
+++ expense-tracker-auth/src/main/java/com/phoenix/expensetrackerservice/constants/AuthConstants.java
@@ -8,4 +8,8 @@ public class AuthConstants {
 
     public static final String AUTH_HEADER = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String[] API_WHITELIST = {
+            ControllerConstants.ACTUATOR_INFO_ENDPOINT,
+            ControllerConstants.ACTUATOR_HEALTH_ENDPOINT
+    };
 }
